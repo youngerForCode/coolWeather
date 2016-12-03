c65a73350ffef05a0d7e14e3f2234af98cce893b
youngerForCode <3327734901@qq.com>
2016-12-03 17:55:56 +0800

    first commit

---
+++ src/com/coolweather/app/LogUtil.java
@@ -0,0 +1,38 @@
+package com.coolweather.app;
+
+import android.util.Log;
+
+public class LogUtil {
+	private static final int VERBOSE = 1;
+	private static final int DEBUG =2;
+	private static final int INFO = 3;
+	private static final int WARN = 4;
+	private static final int ERROR = 5;
+	public static final int LEVEL = VERBOSE;
+	
+	public static void v(String tag,String msg){
+		if(VERBOSE >= LEVEL)
+			Log.v(tag, msg);
+	}
+	
+	public static void d(String tag,String msg){
+		if(DEBUG >= LEVEL)
+			Log.d(tag, msg);
+	}
+	
+	public static void i(String tag,String msg){
+		if(INFO >= LEVEL)
+			Log.i(tag, msg);
+	}
+	
+	public static void w(String tag,String msg){
+		if(WARN >= LEVEL)
+			Log.w(tag, msg);
+	}
+	
+	public static void e(String tag,String msg){
+		if(ERROR >= LEVEL)
+			Log.w(tag, msg);
+	}
+
+}
